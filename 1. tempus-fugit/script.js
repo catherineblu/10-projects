63661e914494ae6fade3d41dc06f8c7633f4c8fd
catherineblu <e.ropleva@gmail.com>
2020-12-20 21:55:05 +0200

    added the seconds const

--- 1. tempus-fugit/script.js
+++ 1. tempus-fugit/script.js
@@ -4,6 +4,8 @@ function countdown (){
     const newYearDate = new Date (newYear);
     const currentDate = new Date();
     console.log(newYearDate - currentDate);
+
+    const seconds = (newYearDate - currentDate) *1000;
 }
 countdown();
 
