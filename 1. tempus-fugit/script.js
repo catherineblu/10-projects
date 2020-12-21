72993759ff1d0664f982dc33d2a27cc69ebca985
catherineblu <e.ropleva@gmail.com>
2020-12-21 11:49:56 +0200

    the countdown function is ready

--- 1. tempus-fugit/script.js
+++ 1. tempus-fugit/script.js
@@ -3,9 +3,16 @@ const newYear = '1 Jan 2021';
 function countdown (){
     const newYearDate = new Date (newYear);
     const currentDate = new Date();
-    console.log(newYearDate - currentDate);
 
-    const seconds = (newYearDate - currentDate) *1000;
+
+    const totalSeconds = new Date (newYearDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    console.log(days, hours, minutes, seconds);
 }
 countdown();
 
