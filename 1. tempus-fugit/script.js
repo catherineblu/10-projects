53e3cfadca3d55de0a2362b63745fca9510086c2
catherineblu <e.ropleva@gmail.com>
2020-12-21 11:57:20 +0200

    success with rendering it in the html

--- 1. tempus-fugit/script.js
+++ 1. tempus-fugit/script.js
@@ -1,18 +1,23 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+
 const newYear = '1 Jan 2021';
 
 function countdown (){
     const newYearDate = new Date (newYear);
     const currentDate = new Date();
-
-
     const totalSeconds = new Date (newYearDate - currentDate) / 1000;
-
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const minutes = Math.floor(totalSeconds / 60) % 60;
     const seconds = Math.floor(totalSeconds) % 60;
 
-    console.log(days, hours, minutes, seconds);
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minutesEl.innerHTML = minutes;
+    secondsEl.innerHTML = seconds;
 }
 countdown();
 
