b89da4bb2b762b9bdde6da9bda2daae9a001c8b8
catherineblu <e.ropleva@gmail.com>
2020-12-21 13:33:33 +0200

    created some javascript dummy questions

---
+++ 2. quizzie-bizzie/scrpit.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old am I?',
+        a: '26',
+        b: '27',
+        c: '28',
+        d: '29',
+        correct: 'b'
+    },{
+        question: 'What I want to become?',
+        a: 'Front-end developer',
+        b: 'Back-end developer',
+        c: 'QA',
+        d: 'DevOPS',
+        correct: 'a'
+    },{
+        question: 'What is my second most favorite thing?',
+        a: 'Counting the stars',
+        b: 'Shopping',
+        c: 'Designing things',
+        d: 'Nothing...',
+        correct: 'c'
+    },{
+        question: 'Which language is back-end?',
+        a: 'CSS',
+        b: 'HTML',
+        c: 'Javascript',
+        d: 'PHP',
+        correct: 'd'
+    },{
+        question: 'What is Bootstrap?',
+        a: 'Programming language',
+        b: 'The most popular HTML, CSS, and JS library in the world.',
+        c: 'A meal?',
+        d: 'I dunno :D',
+        correct: 'b'
+    }
+]
