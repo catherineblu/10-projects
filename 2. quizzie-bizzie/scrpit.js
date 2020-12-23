94784f444441fbca00ee7359ac1507c2a898debb
catherineblu <e.ropleva@gmail.com>
2020-12-23 22:26:53 +0200

    added id to the submit button

--- 2. quizzie-bizzie/scrpit.js
+++ 2. quizzie-bizzie/scrpit.js
@@ -42,6 +42,7 @@ const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit')
 
 let currentQuiz = 0;
 loadQuiz();
