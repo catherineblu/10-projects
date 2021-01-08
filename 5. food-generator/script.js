a30cd160faf6886306a763b4bed5f79e7154cf17
catherineblu <e.ropleva@gmail.com>
2021-01-08 20:26:27 +0200

    connected to the meal db

--- 5. food-generator/script.js
+++ 5. food-generator/script.js
@@ -1,2 +1,10 @@
 const getMealBtn = document.getElementById('get_meal');
 const mealContainer = document.getElementById('meal');
+
+getMealBtn.addEventListener('click', () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(res => res.json())
+    .then(res => {
+    createMeal(res.meals[0])
+    })
+});
