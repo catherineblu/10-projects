d3f6bccb82bb52f96e9f564e90a3c83d259aeea3
catherineblu <e.ropleva@gmail.com>
2021-01-08 21:01:16 +0200

    rendered the heading and the instructions of the meal

--- 5. food-generator/script.js
+++ 5. food-generator/script.js
@@ -1,19 +1,23 @@
-const getMealBtn = document.getElementById('get_meal');
-const mealContainer = document.getElementById('meal');
+const getMealBtn = document.getElementById("get_meal");
+const mealContainer = document.getElementById("meal");
 
-getMealBtn.addEventListener('click', () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-    .then(res => res.json())
-    .then(res => {
-    createMeal(res.meals[0])
-    })
+getMealBtn.addEventListener("click", () => {
+  fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+    .then((res) => res.json())
+    .then((res) => {
+      createMeal(res.meals[0]);
+    });
 });
 
-function createMeal(meal){
-    mealContainer.innerHTML = `
+function createMeal(meal) {
+  mealContainer.innerHTML = `
      <div class="row">
       <div class="column five">
        <img src="${meal.strMealThumb}" alt = "Meal image"/>
+       </div>
+       <div class="column seven">
+       <h4>${meal.strMeal}</h4>
+       <p>${meal.strInstructions}</p>
        </div>
        </div>
     `;
