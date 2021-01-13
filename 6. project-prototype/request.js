f7f75f1238178cccc27320475ba11d20b320364d
catherineblu <e.ropleva@gmail.com>
2021-01-13 14:23:24 +0200

    fetch data from the api

--- 6. project-prototype/request.js
+++ 6. project-prototype/request.js
@@ -1,9 +1,24 @@
 const key = "ad5c4063fad48812931799267dd25a22";
-const baseURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=ad5c4063fad48812931799267dd25a22";
+//const baseURL =
+//  "http://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=ad5c4063fad48812931799267dd25a22";
 
-fetch(baseURL).then((data) => {
-  console.log("response", data.json()) })
-.catch((error)=>{
-    console.log(error)
-})
+//fetch(baseURL).then((data) => {
+//  console.log("response", data.json()) })
+//.catch((error)=>{
+//    console.log(error)
+//});
+
+const requestCity = async (city)=>{
+const baseURL='http://api.openweathermap.org/data/2.5/weather'
+
+const query = `?q=${city}&appid=${key}`;
+
+//fetch call
+
+const response = await fetch(baseURL+query);
+
+//promise data
+const data = await response.json();
+console.log(data);
+}
+requestCity('Abuja');
