d1656df219342edd3df11159e94634c9e0d28ce6
catherineblu <e.ropleva@gmail.com>
2021-01-13 14:02:59 +0200

    fetching data from api

--- 6. project-prototype/request.js
+++ 6. project-prototype/request.js
@@ -1,1 +1,9 @@
 const key = "ad5c4063fad48812931799267dd25a22";
+const baseURL =
+  "http://api.openweathermap.org/data/2.5/weather?q=Lagos&appid=ad5c4063fad48812931799267dd25a22";
+
+fetch(baseURL).then((data) => {
+  console.log("response", data.json()) })
+.catch((error)=>{
+    console.log(error)
+})
