94374dcd12372c7526f9a739f23cfc5e7ebdc859
catherineblu <e.ropleva@gmail.com>
2021-01-14 16:30:25 +0200

    made the icon bigger

--- 6. project-prototype/script.js
+++ 6. project-prototype/script.js
@@ -10,7 +10,7 @@ const toCelsius = (kelvin) =>{
 
 updateWeatherApp = (city)=>{
     const imageName = city.weather[0].icon
-    const iconSrc = `http://openweathermap.org/img/wn/${imageName}.png`
+    const iconSrc = `http://openweathermap.org/img/wn/${imageName}@2x.png`
     cityName.textContent=city.name;
     cardBody.innerHTML=`
     <div class="card-mid row">
